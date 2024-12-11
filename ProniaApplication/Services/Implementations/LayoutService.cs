2cde3d9218f6beacc2d1e0d95ca9731e05c38766
IsmayilGitAccount <ismayilni-bp217@code.edu.az>
2024-12-10 23:09:29 -0800

    Initial commit

---
+++ ProniaApplication/Services/Implementations/LayoutService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using Microsoft.EntityFrameworkCore;
+using ProniaApplication.DAL;
+using ProniaApplication.Services.Interfaces;
+
+namespace ProniaApplication.Services.Impelementations
+{
+    public class LayoutService:ILayoutService
+    {
+        private readonly AppDBContext _context;
+
+        public LayoutService(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Dictionary<string, string>> GetSettingsAsync()
+        {
+            Dictionary<string, string> settings = await _context.Settings.ToDictionaryAsync(s => s.Key, s => s.Value);
+            return settings;
+        }
+    }
+}
---
+++ ProniaApplication/Services/Interfaces/ILayoutService.cs
@@ -0,0 +1,7 @@
+﻿namespace ProniaApplication.Services.Interfaces
+{
+    public interface ILayoutService
+    {
+        Task<Dictionary<string, string>> GetSettingsAsync();
+    }
+}
